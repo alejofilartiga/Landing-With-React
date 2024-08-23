80b729ca14e39d670a5b264a9312d33a39d73e18
Alejo <alejofilartiga@gmail.com>
2024-08-23 20:38:17 -0300

    Product Data

--- src/assets/components/Pages/Products/Products.jsx
+++ src/assets/components/Pages/Products/Products.jsx
@@ -4,27 +4,13 @@ import {
         TitleContainer,
         InfoSection,
         TitleSection,
-        ProductsContainer,
-        Card,
-        CardImg,
-        CardProduct,
-        CardPriceContainer,
-        CardPrice,
-        CardShop
 
 } from './productsStyle'
 
-import CartIcon from './img/carticon.png'
+import { products } from '../../../data/products/productsData'
 
-import Cheddar from './img/taucheddar.jpg'
-import BigTau from './img/bigtau.webp'
-import Barbacoa from './img/barbacoa.png'
-import MayoAjo from './img/mayoajo.webp'
-import MosDulce from './img/mosdulce.jpg'
-import Caesar from './img/taucesar.jpg'
-import Chimichurri from './img/tauchimichurri.jpg'
-import Ranch from './img/tauranch.webp'
-import Teriyaki from './img/teriyaki.webp'
+
+
 
 function Products() {
   return (
@@ -34,72 +20,7 @@ function Products() {
           <TitleSection>Nuestros Exitos</TitleSection>
           <InfoSection>Conoce a nuestros productos estrellas y enamorate junto a ellos en cada comida</InfoSection>
         </TitleContainer>
-        <ProductsContainer>
-          <Card>
-            <CardImg src={Cheddar}/>
-            <CardProduct>Cheddar</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶4̶0̶ $20</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={BigTau}/>
-            <CardProduct>Big Tau</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$40</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={MayoAjo}/>
-            <CardProduct>Mayonesa Ajo</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶4̶0̶ $20</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={MosDulce}/>
-            <CardProduct>Mostaza Dulce</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶4̶0̶ $10</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={Caesar}/>
-            <CardProduct>Caesar</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶5̶0̶  $20</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={Chimichurri}/>
-            <CardProduct>Chimichurri</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶4̶0̶ $20</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={Ranch}/>
-            <CardProduct>Ranch</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$̶4̶0̶ $05</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-          <Card>
-            <CardImg src={Teriyaki}/>
-            <CardProduct>Teriyaki</CardProduct>
-            <CardPriceContainer>
-              <CardPrice>$30</CardPrice>
-              <CardShop src={CartIcon} />
-            </CardPriceContainer>
-          </Card>
-        </ProductsContainer>
+        <ProductList />
       </ProductsSection>
     </>
   )
---
+++ src/assets/data/products/productsData.js
@@ -0,0 +1,59 @@
+import Barbacoa from './img/barbacoa.png';
+import BigTau from './img/bigtau.webp';
+import Mayo from './img/mayoajo.webp'
+import Mostaza from './img/mosdulce.jpg'
+import Caesar from './img/taucesar.jpg'
+import Cheddar from './img/taucheddar.jpg'
+import Chimi from './img/tauchimichurri.jpg'
+import Ranch from './img/tauranch.webp'
+
+export const products = [
+    {
+        id: 1,
+        name: "Barbacoa",
+        img: Barbacoa,
+        price: 1000  
+    },
+    {
+        id: 2,
+        name: "Big Tau",
+        img: BigTau,
+        price: 1500
+    },
+    {
+        id: 3,
+        name: "Mayoajo",
+        img: Mayo,
+        price: 500
+    },
+    {
+        id: 4,
+        name: "Mostaza Dulce",
+        img: Mostaza,
+        price: 700
+    },
+    {
+        id: 5,
+        name: "Tau Caesar",
+        img: Caesar,
+        price: 800
+    },
+    {
+        id: 6,
+        name: "Tau Cheddar",
+        img: Cheddar,
+        price: 900
+    },
+    {
+        id: 7,
+        name: "Tau Chimi",
+        img: Chimi,
+        price: 600
+    },
+    {
+        id: 8,
+        name: "Tau Ranch",
+        img: Ranch,
+        price: 700
+    }
+]
